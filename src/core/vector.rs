805f82a51b244116a16a2e3fd7e1f7a046f56f0a
Sven Liefgen <sven@liefgen.lu>
2020-03-30 15:22:42 +0200

    Add Vector tests

--- src/core/vector.rs
+++ src/core/vector.rs
@@ -1,7 +1,7 @@
 use std::iter;
 use std::ops;
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Vector {
     x: f64,
     y: f64,
@@ -124,5 +124,85 @@ impl ops::Div<f64> for Vector {
 impl ops::DivAssign<f64> for Vector {
     fn div_assign(&mut self, rhs: f64) {
         *self = *self / rhs;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let v = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(v, Vector { x: 1.0, y: 2.0, z: 3.0 });
+    }
+
+    #[test]
+    fn components() {
+        let v = Vector::new(1.0, -3.0, 1000.0);
+        let mut c = v.components();
+        assert_eq!(c.next(), Some(1.0));
+        assert_eq!(c.next(), Some(-3.0));
+        assert_eq!(c.next(), Some(1000.0));
+        assert_eq!(c.next(), None);
+    }
+
+    #[test]
+    fn len() {
+        let v1 = Vector::new(4.0, 3.0, 0.0);
+        let v2 = Vector::new(234.0, 57.0, -4576.0);
+        assert_eq!(v1.len(), 5.0);
+        assert_eq!(v2.len(), ((234*234 + 57*57 + 4576*4576) as f64).sqrt());
+    }
+
+    #[test]
+    fn lensq() {
+        let v = Vector::new(346.0, 135.0, 535.0);
+        assert_eq!(v.lensq(), (346*346 + 135*135 + 535*535) as f64);
+    }
+
+    #[test]
+    fn neg() {
+        let v = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(-v, Vector { x: -1.0, y: -2.0, z: -3.0 });
+    }
+
+    #[test]
+    fn add() {
+        let v1 = Vector::new(1.0, 2.0, 3.0);
+        let v2 = Vector::new(2.0, 4.0, 9.0);
+        let mut v3 = Vector::new(2.0, 6.0, 8.0);
+        v3 += v2 + v1;
+        assert_eq!(v1 + v2, Vector { x: 3.0, y: 6.0, z: 12.0 });
+        assert_eq!(v3, Vector { x: 5.0, y: 12.0, z: 20.0 });
+    }
+
+    #[test]
+    fn sub() {
+        let v1 = Vector::new(1.0, 5.0, 9.0);
+        let v2 = Vector::new(2.0, 0.0, 3.0);
+        let mut v3 = Vector::new(4.0, 1.0, 5.0);
+        v3 -= v2 - v1;
+        assert_eq!(v1 - v2, Vector { x: -1.0, y: 5.0, z: 6.0 });
+        assert_eq!(v3, Vector { x: 3.0, y: 6.0, z: 11.0 });
+    }
+
+    #[test]
+    fn mul() {
+        let v1 = Vector::new(1.0, 5.0, 6.0);
+        let mut v2 = Vector::new(3.0, 2.0, 9.0);
+        v2 *= 1.25;
+        assert_eq!(v1 * 500.0, Vector { x: 500.0, y: 2500.0, z: 3000.0 });
+        assert_eq!(v2, Vector { x: 3.75, y: 2.5, z: 11.25 });
+    }
+
+    #[test]
+    fn div() {
+        let v1 = Vector::new(1.0, 4.0, 2.0);
+        let mut v2 = Vector::new(3.0, 7.0, 2.0);
+        v2 /= 10.0;
+        assert_eq!(v1 / 0.5, Vector { x: 2.0, y: 8.0, z: 4.0 });
+        assert_eq!(v2, Vector { x: 0.3, y: 0.7, z: 0.2 });
     }
 }
