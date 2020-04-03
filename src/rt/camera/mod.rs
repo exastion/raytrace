bd5efe91979a9d64eeb0f1daeccd76ded9d7b1fb
Sven Liefgen <sven@liefgen.lu>
2020-04-03 15:46:53 +0200

    Add PerspectiveCamera

--- src/rt/camera/mod.rs
+++ src/rt/camera/mod.rs
@@ -14,24 +14,48 @@ pub struct GenericCamera {
     y_span: Vector,
 }
 
+/// An orthograpich camera.
 pub struct OrthographicCamera {
-    base: GenericCamera,
+    cam: GenericCamera,
+}
+
+pub struct PerspectiveCamera {
+    cam: GenericCamera,
 }
 
 impl OrthographicCamera {
+    /// Creates a new orthographic camera.
     pub fn new(o: Point, d: Vector, up: Vector, scale_x: f64, scale_y: f64) -> Self {
         let x_span = d.cross(up) * scale_x * 0.5;
         let y_span = x_span.cross(d) * scale_y * 0.5;
         Self {
-            base: GenericCamera { o, d, up, x_span, y_span }
+            cam: GenericCamera { o, d, up, x_span, y_span }
         }
     }
 }
 
 impl Camera for OrthographicCamera {
     fn get_primary_ray(self, x: f64, y: f64) -> Ray {
-        let v = x * self.base.x_span
-              + y * self.base.y_span;
-        Ray::new(self.base.o + v, self.base.d)
+        let v = x * self.cam.x_span
+              + y * self.cam.y_span;
+        Ray::new(self.cam.o + v, self.cam.d)
+    }
+}
+
+impl PerspectiveCamera {
+    /// Creates a new perspective camera.
+    pub fn new(o: Point, d: Vector, up: Vector, h: f64, v: f64) -> Self {
+        let x_span = d.cross(up).norm() * f64::tan(h / 2.0);
+        let y_span = x_span.cross(d).norm() * f64::tan(v / 2.0);
+        Self {
+            cam: GenericCamera { o, d, up, x_span, y_span }
+        }
+    }
+}
+
+impl Camera for PerspectiveCamera {
+    fn get_primary_ray(self, x: f64, y: f64) -> Ray {
+        let v = x * self.cam.x_span + y * self.cam.y_span;
+        Ray::new(self.cam.o, (self.cam.d + v).norm())
     }
 }
